31e0f482a7025aa8c55ccdaccc3af721d847efb2
Brian Dennis <bmd+github@crossjam.net>
2022-12-15 22:12:44 -0500

    Initial commit of subscriptions subcommand

--- feedbin_tools/cli.py
+++ feedbin_tools/cli.py
@@ -1,8 +1,13 @@
+import json
 import logging
+import sys
 
 import click
 
 from .logconfig import DEFAULT_LOG_FORMAT, logging_config
+
+import requests
+import requests_cache
 
 
 @click.group()
@@ -28,10 +33,14 @@ def cli(log_format, log_level, log_file):
     logging_config(log_format, log_level, log_file)
 
 
-@cli.command(name="command")
-@click.argument("example")
-def first_command(example):
+@cli.command(name="subscriptions")
+def first_command():
     "Command description goes here"
 
-    click.echo("Here is some output")
     logging.info("Here's some log output")
+
+    session = requests_cache.CachedSession()
+    resp = session.get("https://api.feedbin.com/v2/subscriptions.json")
+    resp.raise_for_status()
+
+    json.dump(resp.json(), sys.stdout)
