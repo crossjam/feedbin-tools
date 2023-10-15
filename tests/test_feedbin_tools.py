18430ee078856d51a8710b6017df601aa58664d8
Brian Dennis <bmd+github@crossjam.net>
2023-10-14 22:44:29 -0400

    Initial commit of simple test to check for implemented subcommands

--- tests/test_feedbin_tools.py
+++ tests/test_feedbin_tools.py
@@ -8,3 +8,13 @@ def test_version():
         result = runner.invoke(cli, ["--version"])
         assert result.exit_code == 0
         assert result.output.startswith("cli, version ")
+
+
+def test_subcommands_exist():
+    subcommands = ["feed", "starred", "subscriptions"]
+
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        for subcommand in subcommands:
+            result = runner.invoke(cli, [subcommand, "--help"])
+            assert result.exit_code == 0
